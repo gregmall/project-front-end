57b2b99dc932fa5d4aa5c43f708c693abc8fc829
Greg Mall <gregmall157@gmail.com>
2020-08-17 17:44:24 -0700

    some styling

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,12 @@ export default class Header extends Component {
                  <img className="beergif" src="/beerslide.gif" alt="beer" />
                  <h1>Beer</h1>
                  <nav>
+                     <div>
                  <Link to='/create'>Create Beer</Link>
+                 </div>
+                 <div>
                     <Link to='/'>Beer List</Link>
+                </div>
                 </nav>
             </header>
         )
